91258463cd3eb90b5b7aed54a7e6c5af7ee50fa1
fengyusheng <1114915144@qq.com>
2021-12-30 18:10:31 +0800

    [添加spring-interceptor项目]，修改人：方雨生，修改时间：2021-12-30

---
+++ idea/heima_2020/Spring/spring_interceptor/src/main/java/com/fang/controller/TargetController.java
@@ -0,0 +1,17 @@
+package com.fang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TargetController {
+
+    @RequestMapping("target")
+    public ModelAndView target(ModelAndView modelAndView){
+        System.out.println("目标资源执行中");
+        modelAndView.addObject("name","feifei");
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
